61cc024e4d432ae76b0dd1207f1766520f1b7661
Mauricio Zanon <mbz0505@gmail.com>
2018-04-25 00:38:11 -0300

    4

--- Guia 1/Guia 1 ej 4 .c
+++ Guia 1/Guia 1 ej 4 .c
@@ -1,28 +1,28 @@
 // Ejercicio 5 - Punteros y sus posibles valores
-01 int main(int argc, char** argv) {
-02   char vector[] = { 0x9, 32, 25, 010 /* octal */, 200, 09 };
-03
-04   char* p = 0;
-05   char* q = 0x0;
-06 
-07   if ( p == q ) {
-08     printf("q y p son iguales\n");
-09   }
-10
-11   p = vector + 0x6;
-12   q = vector;
-13
-14   p = vector + 5;
-15
-16   if ( p == q ) {
-17     printf("q y p son iguales\n");
-18   }
-19
-20  char* r = vector + 10;
-21  printf("%\n", *r);
-22
-23  return 0;
-24 }
+ int main(int argc, char** argv) {
+   char vector[] = { 0x9, 32, 25, 010 /* octal */, 200, 09 };
+
+   char* p = 0;
+   char* q = 0x0;
+ 
+   if ( p == q ) {
+     printf("q y p son iguales\n");
+   }
+
+   p = vector + 0x6;
+   q = vector;
+
+   p = vector + 5;
+
+   if ( p == q ) {
+     printf("q y p son iguales\n");
+   }
+
+  char* r = vector + 10;
+  printf("%\n", *r);
+
+  return 0;
+ }
 // 1.	Cuál es la diferencia entre p y q?
 // 2.	Se imprime la leyenda q y p son iguales?
 // 3.	Cuanto vale p luego de ejecutar la linea 11? Es válido el puntero?
